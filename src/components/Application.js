73c3d43420311939b4ed97232f4db5fbb023da18
Rongxin Zhang <mrrongxin@gmail.com>
2020-10-21 10:24:00 -0700

    Fix side bar styling

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,9 +21,22 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        <nav>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+
+        <nav className="sidebar__menu">
           <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
+
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {getAppointmentsForDay(state, state.day).map((appointment) => {
