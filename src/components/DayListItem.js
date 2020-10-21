537e27b379a43cf2cce18a18869665a5fccc16de
Rongxin Zhang <mrrongxin@gmail.com>
2020-10-21 10:04:47 -0700

    Revert empty slots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,27 +1,31 @@
 import React from "react";
-import classNames from 'classnames/bind';
-import "components/DayListItem.scss"
+import classNames from "classnames/bind";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots
-  })
+    "day-list__item--full": !props.spots,
+  });
 
-  const formatSpots = ()=>{
+  const formatSpots = () => {
     const spots = props.spots;
-    if (spots < 1){
+    if (spots < 1) {
       return "no spots remaining";
     }
-    if (spots % 2 === 0){
+    if (spots % 2 === 0) {
       return `${props.spots} spots remaining`;
     }
     return `${props.spots} spot remaining`;
-  }
+  };
 
   return (
-    <li data-testid="day" className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li
+      data-testid="day"
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
